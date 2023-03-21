cd6662a87e44a1e91a0d3c414a27793fbc8c5e51
swaroop <swaroop.dixit115@gmail.com>
2023-03-21 12:21:09 +0530

    new

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -13,12 +13,46 @@ public class Calculator {
             ans*=i;
         return ans;
     }
+
+    public double squareRoot(double number1) {
+        logger.info("[SQ ROOT] - " + number1);
+        double result = Math.sqrt(number1);
+        logger.info("[RESULT - SQ ROOT] - " + result);
+        return result;
+    }
+
+    public double power(double number1, double number2) {
+        logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
+        double result = Math.pow(number1,number2);
+        logger.info("[RESULT - POWER] - " + result);
+        return result;
+    }
+
+    public double naturalLog(double number1) {
+        logger.info("[NATURAL LOG] - " + number1);
+        double result = 0;
+        try {
+
+            if (number1 <0 ) {
+                result = Double.NaN;
+                throw new ArithmeticException("Case of NaN 0.0/0.0");
+            }
+            else {
+                result = Math.log(number1);
+            }
+        } catch (ArithmeticException error) {
+            System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
+        }
+        logger.info("[RESULT - NATURAL LOG] - " + result);
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        double number1;
+        double number1,number2;
         do {
             System.out.println("Calculator-DevOps, Choose to perform operation");
             System.out.print("Press 1 to find factorial\nPress 2 to find Square root\nPress 3 to find power\nPress 4 to find natural logarithm\n" +
@@ -35,6 +69,29 @@ public class Calculator {
                     System.out.print("Enter a number : ");
                     number1 = scanner.nextDouble();
                     System.out.println("Factorial of "+number1+" is : " + calculator.factorial(number1));
+                    System.out.println("\n");
+
+                case 2:
+                    // Square root
+                    System.out.print("Enter a number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.println("Square root of "+number1+" is : " + calculator.squareRoot(number1));
+                    System.out.println("\n");
+
+                case 3:
+                    // Power
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.print("Enter the second number : ");
+                    number2 = scanner.nextDouble();
+                    System.out.println(number1+ " raised to power "+number2+" is : " + calculator.power(number1, number2));
+                    System.out.println("\n");
+
+                case 4:
+                    // Natural log
+                    System.out.print("Enter a number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.println("Natural log of "+number1+" is : " + calculator.naturalLog(number1));
                     System.out.println("\n");
 
                 case 5:
--- src/test/java/Caltesting.java
+++ src/test/java/Caltesting.java
@@ -21,4 +21,50 @@ public class Caltesting {
         assertNotEquals("Finding factorial of a number for False Positive", 9, calculator.factorial(2), DELTA);
         assertNotEquals("Finding factorial of a number for False Positive", 0, calculator.factorial(0), DELTA);
     }
+
+    @Test
+    public void powerTruePositive(){
+        assertEquals("Finding power for True Positive", 8, calculator.power(2, 3), DELTA);
+        assertEquals("Finding power for True Positive", 1, calculator.power(1, 3), DELTA);
+        assertEquals("Finding power for True Positive", 81, calculator.power(3, 4), DELTA);
+        assertEquals("Finding power for True Positive", 64, calculator.power(4, 3), DELTA);
+        assertEquals("Finding power for True Positive", 25, calculator.power(5, 2), DELTA);
+    }
+
+    @Test
+    public void powerFalsePositive(){
+        assertNotEquals("Finding power for False Positive", 6, calculator.power(2, 3), DELTA);
+        assertNotEquals("Finding power for False Positive", -7, calculator.power(3, 3), DELTA);
+        assertNotEquals("Finding power for False Positive", 99, calculator.power(4, 3), DELTA);
+        assertNotEquals("Finding power for False Positive", -12, calculator.power(5, 3), DELTA);
+    }
+
+    @Test
+    public void logTruePositive(){
+        assertEquals("Finding natural log for True Positive", 0, calculator.naturalLog(1), DELTA);
+
+    }
+
+    @Test
+    public void logFalsePositive(){
+        assertNotEquals("Finding natural log for False Positive", 60, calculator.naturalLog(2.4), DELTA);
+        assertNotEquals("Finding natural log for False Positive", 4.7, calculator.naturalLog(2.1), DELTA);
+    }
+
+    @Test
+    public void squareRootTruePositive(){
+        assertEquals("Finding square root for True Positive", 4, calculator.squareRoot(16), DELTA);
+        assertEquals("Finding square root for True Positive", 1, calculator.squareRoot(1), DELTA);
+        assertEquals("Finding square root for True Positive", 9, calculator.squareRoot(81), DELTA);
+        assertEquals("Finding square root for True Positive", 6, calculator.squareRoot(36), DELTA);
+    }
+
+    @Test
+    public void squareRootFalsePositive(){
+        assertNotEquals("Finding square root for False Positive", 1, calculator.squareRoot(3), DELTA);
+        assertNotEquals("Finding square root for False Positive", 3, calculator.squareRoot(4), DELTA);
+        assertNotEquals("Finding square root for False Positive", 4, calculator.squareRoot(81), DELTA);
+        assertNotEquals("Finding square root for False Positive", 2, calculator.squareRoot(36), DELTA);
+
+    }
 }
