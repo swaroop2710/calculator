7244ed8ff26dc16a10127279fa0fc66db90386b3
swaroop <swaroop.dixit115@gmail.com>
2023-03-24 15:59:23 +0530

    new

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -47,7 +47,8 @@ public class Calculator {
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main
+            (String[] args) {
         System.out.println("Hello world!");
 
         Calculator calculator = new Calculator();
